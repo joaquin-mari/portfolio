9421e8c6bc01d05f9ba6649104e38c6f71dc8e03
Joaquín <joaquin.mari.marcos@gmail.com>
2025-10-21 15:45:43 +0200

    formatted the text and image in the home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,20 @@
 export default function Home() {
   return (
-    <div className="max-w-lg mx-auto text-left">
-      <div>
-        <p>Hola, my name is</p>
-        <h1>Joaquin Mari.</h1>
-        <h2>Software Engineer</h2>
-        <h3>
+    <div className="max-w-5xl gap-10 mx-auto text-left flex pt-20">
+      <div className="space-y-5 pt-10">
+        <p className="text-cool-cyan">Hola, my name is</p>
+        <div className="font-playfair space-y-5">
+          <h1 className="font-bold text-7xl">Joaquin Mari.</h1>
+          <h2 className="font-semibold text-5xl text-text-muted">
+            Software Engineer
+          </h2>
+        </div>
+        <h3 className="text-text-muted">
           I am a last year university student and part time web developer at
           Airbus
         </h3>
       </div>
+      <img src="/picture.png" alt="portfolio picture" className="w-100" />
     </div>
   );
 }
