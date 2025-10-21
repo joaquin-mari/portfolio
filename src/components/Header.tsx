9dcf80b4959252460194c7af5036438399336e3d
Joaquín <joaquin.mari.marcos@gmail.com>
2025-10-21 15:47:36 +0200

    hover on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,20 @@ export default function Header() {
     <header className="w-screen bg-yellow flex justify-between px-10 py-5  font-playfair">
       <img src="/logo.png" alt="Icon Image" className="w-13 h-10" />
       <div className="flex gap-5 items-center">
-        <ol className="flex gap-5">
-          <li>
+        <ol className="flex gap-5 ">
+          <li className="hover:text-cool-cyan cursor-pointer">
             <NumberSpan text="1" />
             About
           </li>
-          <li>
+          <li className="hover:text-cool-cyan cursor-pointer">
             <NumberSpan text="2" />
             Experience
           </li>
-          <li>
+          <li className="hover:text-cool-cyan cursor-pointer">
             <NumberSpan text="3" />
             Work
           </li>
-          <li>
+          <li className="hover:text-cool-cyan cursor-pointer">
             <NumberSpan text="4" />
             Contact
           </li>
