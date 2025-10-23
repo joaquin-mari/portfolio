5b94d05acc3778f8d36c6bbbef8e76b340d20728
Joaquín <joaquin.mari.marcos@gmail.com>
2025-10-23 11:23:50 +0200

    left banner finished

--- src/components/LeftBanner.tsx
+++ src/components/LeftBanner.tsx
@@ -2,7 +2,9 @@ import VerticalLine from "./VerticalLine";
 
 export default function LeftBanner() {
   return (
-    <div className="flex flex-col absolute left-10 bottom-0">
+    <div className="absolute flex flex-col absolute left-10 bottom-0 items-center gap-3">
+      <img className="cursor-pointer" src="/GitHub.png" alt="github link" />
+      <img className="cursor-pointer" src="/LinkedIn.png" alt="linkedin link" />
       <VerticalLine height={"h-52"} />
     </div>
   );
