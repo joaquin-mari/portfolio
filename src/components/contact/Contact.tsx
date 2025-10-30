228d290fa60de1222c8df74eaf55ed61c8a454ed
Joaquín <joaquin.mari.marcos@gmail.com>
2025-10-30 17:43:15 +0100

    finished contact

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,20 @@
+import CyanButton from "../utils/CyanButton";
+
+export default function Contact() {
+  return (
+    <section className="flex items-center justify-center min-h-screen w-full flex-col text-center gap-5">
+      <span className="text-cool-cyan text-inter text-xl">
+        o.4 What's next?
+      </span>
+      <h2 className="font-playfair text-6xl font-bold">Contact Me</h2>
+      <div className="w-180">
+        <p className="text-gray-border text-2xl mb-10">
+          I am looking for new challenges and opportunities so please feel free
+          to contact me with any idea, question or thing you would like to share
+          with me!
+        </p>
+      </div>
+      <CyanButton text={"Say Hello"} size="lg" />
+    </section>
+  );
+}
--- src/components/utils/CyanButton.tsx
+++ src/components/utils/CyanButton.tsx
@@ -1,9 +1,19 @@
 interface Props {
   text: string;
+  size?: "sm" | "md" | "lg";
 }
-export default function CyanButton({ text }: Props) {
+
+export default function CyanButton({ text, size = "md" }: Props) {
+  const sizeClasses = {
+    sm: "px-3 py-1 text-sm",
+    md: "px-5 py-2 text-md",
+    lg: "px-8 py-3 text-lg",
+  };
+
   return (
-    <button className="px-5 py-2 border border-cool-cyan rounded border-2 text-cool-cyan cursor-pointer transition-all duration-300 ease-in-out hover:bg-cool-cyan hover:text-white active:scale-95q">
+    <button
+      className={`border-2 border-cool-cyan rounded text-cool-cyan cursor-pointer transition-all duration-300 ease-in-out hover:bg-cool-cyan hover:text-white active:scale-95 ${sizeClasses[size]}`}
+    >
       {text}
     </button>
   );
