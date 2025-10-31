93ddcb14b773d91d4d8f53c80adf541af33f8a86
Joaquín <joaquin.mari.marcos@gmail.com>
2025-10-31 18:53:25 +0100

    left banner buttons

--- src/components/side-banners/LeftBanner.tsx
+++ src/components/side-banners/LeftBanner.tsx
@@ -3,8 +3,24 @@ import VerticalLine from "./VerticalLine";
 export default function LeftBanner() {
   return (
     <div className="fixed flex flex-col absolute left-10 bottom-0 items-center gap-3">
-      <img className="cursor-pointer" src="/GitHub.png" alt="github link" />
-      <img className="cursor-pointer" src="/LinkedIn.png" alt="linkedin link" />
+      <a
+        href="https://github.com/joaquin-mari"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="cursor-pointer" src="/GitHub.png" alt="github link" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/joaquin-mari-marcos/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="cursor-pointer"
+          src="/LinkedIn.png"
+          alt="linkedin link"
+        />
+      </a>
       <VerticalLine height={"h-65"} />
     </div>
   );
