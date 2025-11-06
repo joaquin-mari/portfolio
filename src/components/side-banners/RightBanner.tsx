9f16ec70d7b27bc778eccc611a04ef112f62888d
Joaquín <joaquin.mari.marcos@gmail.com>
2025-11-06 18:07:18 +0100

    link to email on right banner

--- src/components/side-banners/RightBanner.tsx
+++ src/components/side-banners/RightBanner.tsx
@@ -2,9 +2,15 @@ import VerticalLine from "./VerticalLine";
 import VerticalText from "./VerticalText";
 export default function RightBanner() {
   return (
-    <div className="flex flex-col gap-3 fixed right-10 bottom-0 items-center">
-      <VerticalText text="joaquin.mari.marcos@gmail.com" />
-      <VerticalLine height="h-20" />
-    </div>
+    <a
+      href="mailto:joaquin.mari.marcos@gmail.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="flex flex-col gap-3 fixed right-10 bottom-0 items-center">
+        <VerticalText text="joaquin.mari.marcos@gmail.com" />
+        <VerticalLine height="h-20" />
+      </div>
+    </a>
   );
 }
